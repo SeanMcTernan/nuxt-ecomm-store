b00a13a6259cebb12dbe7a192112569ed3bca373
Sean Mc Ternan <seanmcternan@gmail.com>
2025-04-18 12:06:28 -0400

    Remove Cypress component testing configuration and files

--- cypress.config.js
+++ cypress.config.js
@@ -5,17 +5,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:8888/',
-    experimentalSessionAndOrigin: true,
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
-  component: {
-    devServer: {
-      framework: 'vue',
-      bundler: 'vite',
-    },
-    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/component.ts',
   },
   video: false,
   screenshotOnRunFailure: true,
